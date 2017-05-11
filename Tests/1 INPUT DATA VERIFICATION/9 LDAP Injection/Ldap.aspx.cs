a41f529509a09baf60c512a45b9351ba6b116561
We are hire! <pt.sdl.qa@gmail.com>
2017-05-11 12:02:17 +0700

    [XSS] in [Ldap.aspx.cs]

--- Tests/1 INPUT DATA VERIFICATION/9 LDAP Injection/Ldap.aspx.cs
+++ Tests/1 INPUT DATA VERIFICATION/9 LDAP Injection/Ldap.aspx.cs
@@ -39,18 +39,26 @@ namespace Mopas.Tests
 
             // this is our first vulnerability of XSS in this file
             // we will demonstrate False Positive scenario here (FP Marker)
+            // TODO: AI issue #109, High, Cross-site Scripting, https://github.com/PTSDLQA/XS-CSharp/issues/109
+            // (Mopas.Tests.Ldap.Request.Params["name"] == "<script>alert(1)</script>")
             Response.Write(name);
 
             // this is our second vulnerability of XSS in this file
             // we will demonstrate what happen if developer fails with his fix (VERIFY Marker)
+            // TODO: AI issue #109, High, Cross-site Scripting, https://github.com/PTSDLQA/XS-CSharp/issues/109
+            // (Mopas.Tests.Ldap.Request.Params["name"] == "<script>alert(1)</script>")
             Response.Write(name);
 
             // this is our third vulnerability of XSS in this file
             // we will demonstrate what happen if we really fix vulnerability (VERIFY Marker)
+            // TODO: AI issue #109, High, Cross-site Scripting, https://github.com/PTSDLQA/XS-CSharp/issues/109
+            // (Mopas.Tests.Ldap.Request.Params["name"] == "<script>alert(1)</script>")
             Response.Write(name);
 
             // this is our fourth vulnerability of XSS in this file
             // we will demonstrate what happen if developer want to cheat (FIXED Marker)
+            // TODO: AI issue #109, High, Cross-site Scripting, https://github.com/PTSDLQA/XS-CSharp/issues/109
+            // (Mopas.Tests.Ldap.Request.Params["name"] == "<script>alert(1)</script>")
             Response.Write(name);
         }
     }

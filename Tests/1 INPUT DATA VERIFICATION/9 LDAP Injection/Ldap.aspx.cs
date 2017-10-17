7548eabcbcb09770ed6691a6202948f14607dd91
We are hire! <pt.sdl.qa@gmail.com>
2017-10-17 13:47:51 +0700

    [XSS] in [Ldap.aspx.cs]

--- Tests/1 INPUT DATA VERIFICATION/9 LDAP Injection/Ldap.aspx.cs
+++ Tests/1 INPUT DATA VERIFICATION/9 LDAP Injection/Ldap.aspx.cs
@@ -23,7 +23,7 @@ namespace Mopas.Tests
             var domain = Domain.GetDomain(dc);
 			
 
-            // this is our vulnerabilitiy of LDAP injection *in this file* 
+            // this is our vulnerabilitiy of LDAP injection *in this file*
             var ds = new DirectorySearcher(domain.GetDirectoryEntry(), filter);
 
             using (var src = ds.FindAll())
@@ -36,30 +36,30 @@ namespace Mopas.Tests
 
             var name = Request.Params["name"];
 
-            // this is our first vulnerability of XSS in this file 
+            // this is our first vulnerability of XSS in this file
             // we will demonstrate False Positive scenario here (FP Marker)
-            // FP: AI issue #19, Medium, XSS, http://desktop-9pr2t70/#/taskResults/13
+            // TODO: AI issue #4, Medium, XSS, http://desktop-9pr2t70/#/taskResults/4
             // GET /Tests/1%20INPUT%20DATA%20VERIFICATION/9%20LDAP%20Injection/Ldap.aspx.cs?name=%3cscript%3ealert(1)%3c%2fscript%3e HTTP/1.1
             // Host: localhost
             Response.Write(name);
 
             // this is our second vulnerability of XSS in this file
             // we will demonstrate what happen if developer fails with his fix (VERIFY Marker)
-            // REOPEN: AI issue #20, Medium, XSS, http://desktop-9pr2t70/#/taskResults/12
+            // TODO: AI issue #3, Medium, XSS, http://desktop-9pr2t70/#/taskResults/3
             // GET /Tests/1%20INPUT%20DATA%20VERIFICATION/9%20LDAP%20Injection/Ldap.aspx.cs?name=%3cscript%3ealert(1)%3c%2fscript%3e HTTP/1.1
             // Host: localhost
             Response.Write(name);
 
             // this is our third vulnerability of XSS in this file
             // we will demonstrate what happen if we really fix vulnerability (VERIFY Marker)
-            // FIXED: AI issue #21, Medium, XSS, http://desktop-9pr2t70/#/taskResults/7
+            // TODO: AI issue #2, Medium, XSS, http://desktop-9pr2t70/#/taskResults/2
             // GET /Tests/1%20INPUT%20DATA%20VERIFICATION/9%20LDAP%20Injection/Ldap.aspx.cs?name=%3cscript%3ealert(1)%3c%2fscript%3e HTTP/1.1
             // Host: localhost
-            Response.Write("name");
+            Response.Write(name);
 
             // this is our fourth vulnerability of XSS in this file
             // we will demonstrate what happen if developer want to cheat (FIXED Marker)
-            // REOPEN: AI issue #22, Medium, XSS, http://desktop-9pr2t70/#/taskResults/11
+            // TODO: AI issue #1, Medium, XSS, http://desktop-9pr2t70/#/taskResults/1
             // GET /Tests/1%20INPUT%20DATA%20VERIFICATION/9%20LDAP%20Injection/Ldap.aspx.cs?name=%3cscript%3ealert(1)%3c%2fscript%3e HTTP/1.1
             // Host: localhost
             Response.Write(name);

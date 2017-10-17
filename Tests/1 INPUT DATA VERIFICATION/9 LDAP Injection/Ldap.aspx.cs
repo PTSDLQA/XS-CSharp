efea043a66c2090ecb88157ecc40a8f2ba469a76
We are hire! <pt.sdl.qa@gmail.com>
2017-10-17 12:27:41 +0700

    [XSS] in [Ldap.aspx.cs]

--- Tests/1 INPUT DATA VERIFICATION/9 LDAP Injection/Ldap.aspx.cs
+++ Tests/1 INPUT DATA VERIFICATION/9 LDAP Injection/Ldap.aspx.cs
@@ -38,18 +38,30 @@ namespace Mopas.Tests
 
             // this is our first vulnerability of XSS in this file
             // we will demonstrate False Positive scenario here (FP Marker)
+            // TODO: AI issue #9, Medium, XSS, http://desktop-9pr2t70/#/taskResults/9
+            // GET /Tests/1%20INPUT%20DATA%20VERIFICATION/9%20LDAP%20Injection/Ldap.aspx.cs?name=%3cscript%3ealert(1)%3c%2fscript%3e HTTP/1.1
+            // Host: localhost
             Response.Write(name);
 
             // this is our second vulnerability of XSS in this file
             // we will demonstrate what happen if developer fails with his fix (VERIFY Marker)
+            // TODO: AI issue #8, Medium, XSS, http://desktop-9pr2t70/#/taskResults/8
+            // GET /Tests/1%20INPUT%20DATA%20VERIFICATION/9%20LDAP%20Injection/Ldap.aspx.cs?name=%3cscript%3ealert(1)%3c%2fscript%3e HTTP/1.1
+            // Host: localhost
             Response.Write(name);
 
             // this is our third vulnerability of XSS in this file
             // we will demonstrate what happen if we really fix vulnerability (VERIFY Marker)
+            // TODO: AI issue #7, Medium, XSS, http://desktop-9pr2t70/#/taskResults/7
+            // GET /Tests/1%20INPUT%20DATA%20VERIFICATION/9%20LDAP%20Injection/Ldap.aspx.cs?name=%3cscript%3ealert(1)%3c%2fscript%3e HTTP/1.1
+            // Host: localhost
             Response.Write(name);
 
             // this is our fourth vulnerability of XSS in this file
             // we will demonstrate what happen if developer want to cheat (FIXED Marker)
+            // TODO: AI issue #6, Medium, XSS, http://desktop-9pr2t70/#/taskResults/6
+            // GET /Tests/1%20INPUT%20DATA%20VERIFICATION/9%20LDAP%20Injection/Ldap.aspx.cs?name=%3cscript%3ealert(1)%3c%2fscript%3e HTTP/1.1
+            // Host: localhost
             Response.Write(name);
         }
     }
